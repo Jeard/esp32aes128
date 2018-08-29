b27f41200d589c0e1181f15f6d7001df404934ab
A N B J <asitha@ieee.org>
2018-08-29 10:54:45 +0530

    Array initialization using malloc + improvements to the structure+ samall bug fix to example

--- examples/encryption_test/encryption_test.ino
+++ examples/encryption_test/encryption_test.ino
@@ -3,7 +3,7 @@
 __AesB64 aesB64;
 
 uint64_t chipid;
-char chip_ID [15];
+char chip_ID [40];
 uint16_t current;
 uint16_t voltge;
 char encryption_key[] = "bse64aes128esp32";
@@ -13,7 +13,7 @@ char encryption_key[] = "bse64aes128esp32";
 void setup() {
   Serial.begin(115200);
   chipid = ESP.getEfuseMac();
-  sprintf(chip_ID, "ESP32 Chipid= %5i%10i", (uint16_t)(chipid >> 32), (uint32_t)(chipid));
+  sprintf(chip_ID, "ESP32 Chipid= %i%i", (uint16_t)(chipid >> 32), (uint32_t)(chipid));
   String encrypted_str = aesB64.encry_charry(chip_ID,encryption_key);
   Serial.println(encrypted_str);}
 
--- src/ESP32AESB64.cpp
+++ src/ESP32AESB64.cpp
@@ -1,4 +1,12 @@
+#include <Arduino.h>
+#include <mbedtls/aes.h>
 #include "ESP32AESB64.h"
+#include "base64.h"
+
+
+
+base64 b_64;
+mbedtls_aes_context aes;
 
 String __AesB64::encry_charry(char *charry, char * key) {
   _len_charry = strlen(charry);
@@ -8,19 +16,19 @@ String __AesB64::encry_charry(char *charry, char * key) {
   if (_modulo_16 != 0) {
     _k= _k+ 1;
     _len_padding = 16 - _modulo_16;}
-  char array_2_encr [_k* 16];
+  char* array_2_encr=(char*) malloc((_k* 16));
   memcpy (array_2_encr, charry, _len_charry );
   if (_modulo_16 != 0) {
     add_padding(array_2_encr, _len_padding, _len_charry );}
-   uint8_t encrypted_array [(_k* 16) + 1]; 
+   uint8_t* encrypted_array=(uint8_t*) malloc((_k* 16) + 1); 
     for (int i = 0; i < _k ;i++) {
-    char block [16];
+    char* block =(char*)malloc(16);
     memcpy(block, array_2_encr + ((16 * i)), 16);
-    char *input = block;
-    uint8_t output[16];
+    //char *input = block;
+    uint8_t* output=(uint8_t*)malloc(16);
     mbedtls_aes_init( &aes );
     mbedtls_aes_setkey_enc( &aes, (const unsigned char*) key, strlen(key) * 8 );
-    mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, (const unsigned char*)input, output);
+    mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, (const unsigned char*)block, output);
     mbedtls_aes_free( &aes );
     memcpy(encrypted_array + (16 * i), output, 16);}
     return (b_64.encode(encrypted_array, (_k* 16)));}
